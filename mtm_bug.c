7de53cd69b653a07f9e350100894831c6ef82f0f
Haj Yahya Raneen <raneen.haj@campus.technion.ac.il>
2024-06-22 21:03:57 +0300

    Add files via upload

--- mtm_bug.c
+++ mtm_bug.c
@@ -33,96 +33,94 @@ void printResults(char** words, int size);
 //==============================================================================
 
 void swap(char** a, char** b) {
-    char* temp = *a;
-    *a = *b;
-    *b = temp;
+	char* temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 char* getLongestString(char** strings, int size) {
-    char* max = strings[0];
-    for (int i = 1; i < size; i++) {
-        if (strlen(max) < strlen(strings[i])) {
-            max = strings[i];
-        }
-    }
-    return max;
+	char* max = NULL;
+	for (int i = 0; i < size; i++) {
+		if (strlen(max) < strlen(strings[i])) {
+			max = strings[i];
+		}
+	}
+	return max;
 }
 
 void sortStrings(char** strings, int size) {
-    bool changed = true;
-    while (changed) {
-        changed = false;
-        for (int i = 0; i < size - 1; i++) {
-            if (strcmp(strings[i], strings[i + 1]) >= 0) {
-                swap(&strings[i], &strings[i + 1]);
-                changed = true;
-            }
-        }
-    }
+	bool changed = true;
+	while (changed) {
+		changed = false;
+		for (int i = 0; i < size - 1; i++) {
+			if (strcmp(strings[i], strings[i + 1]) >= 0) {
+				swap(&strings[i], &strings[i + 1]);
+				changed = true;
+			}
+		}
+	}
 }
 
 int readSize() {
-    int size = 0;
-    printf("Enter number of strings:\n");
-    scanf("%d", &size);
-    return size;
+	int size = 0;
+	printf("Enter number of strings:\n");
+	scanf("%d", &size);
+	return size;
 }
 
 void printResults(char** words, int size) {
-    char* longest = getLongestString(words, size);
-    printf("The longest word is: %s\n", longest);
-    sortStrings(words, size);
-    printf("The maximal word lexicographically is: %s\n", words[size-1]);
-    printf("The minimal word lexicographically is: %s\n", words[0]);
+	char* longest = getLongestString(words, size);
+	printf("The longest word is: %s\n", longest);
+	sortStrings(words, size);
+	printf("The maximal word lexicographically is: %s\n", words[size]);
+	printf("The minimal word lexicographically is: %s\n", words[0]);
 }
 
 char* readWord() {
-    char buffer[BUFFER_SIZE] = "";
-    scanf("%s", buffer);
-    if (strlen(buffer) < 1) {
-        return NULL;
-    }
-    char* str = malloc(strlen(buffer) + 1);
-    if (!str) {
-        return NULL;
-    }
-    return strcpy(str,buffer);
+	char buffer[BUFFER_SIZE] = "";
+	scanf("%s", buffer);
+	if (strlen(buffer) < 1) {
+		return NULL;
+	}
+	char* str = malloc(strlen(buffer) + 1);
+	if (!str) {
+		return NULL;
+	}
+	return strcpy(str,buffer);
 }
 
 char** readWords(int size) {
-    char** words = malloc(size * sizeof(*words));
-    if (words == NULL) {
-        return NULL;
-    }
-    for (int i = 0; i < size; ++i) {
-        words[i] = readWord();
-        if (words[i] == NULL) {
-            freeWords(words, size);
-            return NULL;
-        }
-    }
-    return words;
+	char** words = malloc(size * sizeof(*words));
+	if (words == NULL) {
+		return NULL;
+	}
+	for (int i = 0; i < size; ++i) {
+		words[i] = readWord();
+		if (words[i] == NULL) {
+			freeWords(words, size);
+			return NULL;
+		}
+	}
+	return words;
 }
 
 void freeWords(char** words, int size) {
-    for (int i = 0; i < size; i++) {
-        free(words[i]);
-    }
-    free(words);
+	for (int i = 0; i < size; i++) {
+		free(words[i]);
+	}
 }
 
 int main() {
-    int size = readSize();
-    if (size < 1) {
-        printf("Invalid size\n");
-        return 0;
-    }
-    char** words = readWords(size);
-    if (words == NULL) {
-        printf("Error reading words\n");
-        return 0;
-    }
-    printResults(words, size);
-    freeWords(words, size);
-    return 0;
+	int size = readSize();
+	if (size < 1) {
+		printf("Invalid size\n");
+	}
+	char** words = readWords(size);
+	if (words == NULL) {
+		printf("Error reading words\n")
+		return 0;
+	}
+	printResults(words, size);
+	freeWords(words, size);
+	return 0;
 }
